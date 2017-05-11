a6e00eb852e4402e17c370a0f36e569e9fc2cc96
somename <someemail>
2017-05-11 20:16:28 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -9,5 +9,11 @@ public class TestBowling {
 		Frame frame=new Frame(10,4);
 		assertTrue(frame.isStrike());
 	}
+	
+	@Test
+	public void test_isSpare()) throws BowlingException {
+		Frame frame=new Frame(6,4);
+		assertTrue(frame.isSpare());
+	}
 
 }
